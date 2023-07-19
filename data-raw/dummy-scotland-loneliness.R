302914b2c29bc9fe17ffcdadbe823b00f2b1eac8
MatteoLarrode <matteo.larrode@orange.fr>
2023-07-19 12:42:29 +0100

    created dummy dataset

--- data-raw/dummy-scotland-loneliness.R
+++ data-raw/dummy-scotland-loneliness.R
@@ -1,10 +1,18 @@
 library(dplyr)
+library(geographr)
+library(sf)
+
+iz <- boundaries_iz11 |>
+  sf::st_drop_geometry()
+
+# Set seed for reproducibility when generating random loneliness_zscore
+set.seed(123)
 
 dummy_scotland_loneliness <- tibble(
-  area_name = NA,
-  zscore = NA,
-  rank =  NA,
-  decile = NA
+  interzone = iz$iz11_name,
+  loneliness_zscore = rnorm(length(interzone)),
+  ranked = rank(loneliness_zscore),
+  deciles = ntile(loneliness_zscore, 10)
 )
 
 usethis::use_data(dummy_scotland_loneliness, overwrite = TRUE)
