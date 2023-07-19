e7c0bba83930233d2c4bdd103498656fe604ac6b
MatteoLarrode <matteo.larrode@orange.fr>
2023-07-19 14:20:35 +0100

    renamed files

--- data-raw/dummy-loneliness-scotland-iz22.R
+++ data-raw/dummy-loneliness-scotland-iz22.R
@@ -8,11 +8,11 @@ iz <- boundaries_iz11 |>
 # Set seed for reproducibility when generating random loneliness_zscore
 set.seed(123)
 
-dummy_scotland_loneliness <- tibble(
+dummy_loneliness_scotland_iz22 <- tibble(
   iz_code11 = iz$iz11_code,
   loneliness_zscore = rnorm(length(iz_code11)),
   rank = rank(loneliness_zscore),
   deciles = ntile(loneliness_zscore, 10)
 )
 
-usethis::use_data(dummy_scotland_loneliness, overwrite = TRUE)
+usethis::use_data(dummy_loneliness_scotland_iz22, overwrite = TRUE)
