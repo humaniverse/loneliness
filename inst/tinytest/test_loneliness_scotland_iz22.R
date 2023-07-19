5fee01de503ee89af913dd024a08c5028489c52b
MatteoLarrode <matteo.larrode@orange.fr>
2023-07-19 15:04:35 +0100

    added tests for columns class

--- inst/tinytest/test_loneliness_scotland_iz22.R
+++ inst/tinytest/test_loneliness_scotland_iz22.R
@@ -5,14 +5,51 @@ pkgload::load_all(".")
 loneliness_scotland_iz <-
   dummy_loneliness_scotland_iz22
 
-# Check its class as a tibble data frame
+# ---- Tests: Overall dataframe ----
+# Test its class as a tibble data frame
 expect_equal(
   class(loneliness_scotland_iz)[1],
   "tbl_df"
 )
 
-# Check its dimensions: 1279 interzones, and 4 columns
+# Test its dimensions: 1279 interzones, and 4 columns
 expect_equal(
   dim(loneliness_scotland_iz),
   c(1279, 4)
 )
+
+# ---- Tests: Interzone ----
+# Test class
+expect_equal(
+  class(loneliness_scotland_iz$iz_code11),
+  "character"
+)
+
+# Test all Interzone codes are there
+
+# ---- Tests: Loneliness z-score ----
+# Test class
+expect_equal(
+  class(loneliness_scotland_iz$loneliness_zscore),
+  "numeric"
+)
+
+# Test all values fall within threshold of sd's
+
+# ---- Tests: Rank ----
+# Test class
+expect_equal(
+  class(loneliness_scotland_iz$rank),
+  "numeric"
+)
+
+# Test unique ranks, from 1 to 1279. If tie: rank is average of values tied
+
+# ---- Tests: Deciles ----
+# Test class
+expect_equal(
+  class(loneliness_scotland_iz$deciles),
+  "integer"
+)
+
+# Test number of values per bin
