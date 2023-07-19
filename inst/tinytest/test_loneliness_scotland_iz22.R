1fb3bdaa3dd224ba106beac3d31c2bb6a757d5f2
MatteoLarrode <matteo.larrode@orange.fr>
2023-07-19 15:36:32 +0100

    created tests for interzone column

--- inst/tinytest/test_loneliness_scotland_iz22.R
+++ inst/tinytest/test_loneliness_scotland_iz22.R
@@ -1,3 +1,5 @@
+library(geographr)
+library(sf)
 pkgload::load_all(".")
 
 # Load dataframe
@@ -26,6 +28,15 @@ expect_equal(
 )
 
 # Test all Interzone codes are there
+scotland_iz_codes <-
+  boundaries_iz11 |> 
+  sf::st_drop_geometry() |>
+  select(iz11_code)
+
+expect_equal(
+  sort(loneliness_scotland_iz$iz_code11),
+  sort(scotland_iz_codes$iz11_code)
+)
 
 # ---- Tests: Loneliness z-score ----
 # Test class
