fb7cb037a7b63ad507cf593dda319984dcbe92f0
MatteoLarrode <matteo.larrode@orange.fr>
2023-07-19 14:33:08 +0100

    created first tests

--- inst/tinytest/test_loneliness_scotland_iz22.R
+++ inst/tinytest/test_loneliness_scotland_iz22.R
@@ -1,4 +1,18 @@
 pkgload::load_all(".")
 
-loneliness_scotland_iz22 <-
-  
+# Load dataframe
+# For now, use dummy dataset, to be changed when data is pushed
+loneliness_scotland_iz <-
+  dummy_loneliness_scotland_iz22
+
+# Check its class as a tibble data frame
+expect_equal(
+  class(loneliness_scotland_iz)[1],
+  "tbl_df"
+)
+
+# Check its dimensions: 1279 interzones, and 4 columns
+expect_equal(
+  dim(loneliness_scotland_iz),
+  c(1279, 4)
+)
