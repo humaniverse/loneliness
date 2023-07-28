aed528be0b4b0ea60ebf0345138e6c907c742423
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2023-07-28 16:40:02 +0200

    fix typo

--- inst/r/lookup_feather.R
+++ inst/r/lookup_feather.R
@@ -3,4 +3,4 @@ library(feather)
 
 lookup_11 <- lookup_postcode_oa11_lsoa11_msoa11_ltla20
 
-write_feather(lookup_11, "inst/extdata/lookup_11.feather>")
+write_feather(lookup_11, "inst/extdata/lookup_11.feather")
