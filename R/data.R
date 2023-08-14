1247fb7fc77cc711791b90d8d28c02879c11622d
jennajt <jennajltan@gmail.com>
2023-08-14 16:17:20 +0100

    updated documentation

--- R/data.R
+++ R/data.R
@@ -1,13 +1,18 @@
 #' England Loneliness Scores from Community Life Survey 2020-21
 #'
-#' A dataset...
+#' A dataset with scores for loneliness at LSOA level.
+#' Scores are a percentage of answers to a direct question on loneliness 
+#' asked in the Community Life Survey conducted by the DCSM.
+#' The question is "How often do you feel lonely?" and the percentage represents
+#' responses of "Often/always & some of the time".
 #' ...
 #'
 #' @format A data frame with 33,755 rows and 2 variables:
 #' \describe{
 #'   \item{lsoa21_code}{Lower Super Output Area 2021 Code}
-#'   \item{perc}{Perc}
+#'   \item{perc}{Percentage that respond "Often/always & some of the time" to
+#'   "How often do you feel lonely?"}
 #'   ...
 #' }
-#' @source \url{...}
+#' @source \url{https://www.gov.uk/government/statistical-data-sets/dcms-community-life-survey-ad-hoc-statistical-releases}
 "england_cls_loneliness_2020"
