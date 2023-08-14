19552675ca9c9573270edbe168f03799a3b53798
jennajt <jennajltan@gmail.com>
2023-08-14 15:59:26 +0100

    Added .RDA file and updated documentation with dummy text

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' England Loneliness Scores from Community Life Survey 2020-21
+#'
+#' A dataset...
+#' ...
+#'
+#' @format A data frame with 33,755 rows and 2 variables:
+#' \describe{
+#'   \item{lsoa21_code}{Lower Super Output Area 2021 Code}
+#'   \item{perc}{Perc}
+#'   ...
+#' }
+#' @source \url{...}
+"england_cls_loneliness_2020"
